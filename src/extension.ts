26a9ac1e7babb7e4343834a4683391ee7a71d2fa
Ben Scholtens <ben.scholtens@me.com>
2023-09-20 12:19:22 -0400

    run prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,42 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { promptAgent, updateLexicon, parseApiResponse } from './utils';
+import * as vscode from "vscode";
+import { promptAgent, updateLexicon, parseApiResponse } from "./utils";
 
-const dotenv = require('dotenv');
-const path = require('path');
+const dotenv = require("dotenv");
+const path = require("path");
 
-const result = dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+const result = dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 if (result.error) {
-    throw result.error;
+  throw result.error;
 }
 
 if (!process.env.OPENAI_API_KEY) {
-    throw new Error("Required environment variables are not set");
+  throw new Error("Required environment variables are not set");
 }
 
 // const userQuery = `The right way to say God is with an uppercase "G"`
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	let disposable = vscode.commands.registerCommand('genesis-translator.helloWorld', async () => {
-		vscode.window.showInputBox({
-			prompt: 'Enter some text',
-			placeHolder: 'Your placeholder here',
-		}).then(async userQuery => {
-			if (userQuery === undefined || userQuery === '') {
-				vscode.window.showInformationMessage('No input provided');
-			} else {
-				if(vscode.workspace.workspaceFolders !== undefined) {
-					const rootPath = vscode.workspace.workspaceFolders[0].uri;
-					// const pattern = new vscode.RelativePattern(rootPath, '**/*');
-					try {
-						const prompt = `
+  let disposable = vscode.commands.registerCommand(
+    "genesis-translator.helloWorld",
+    async () => {
+      vscode.window
+        .showInputBox({
+          prompt: "Enter some text",
+          placeHolder: "Your placeholder here",
+        })
+        .then(async (userQuery) => {
+          if (userQuery === undefined || userQuery === "") {
+            vscode.window.showInformationMessage("No input provided");
+          } else {
+            if (vscode.workspace.workspaceFolders !== undefined) {
+              const rootPath = vscode.workspace.workspaceFolders[0].uri;
+              // const pattern = new vscode.RelativePattern(rootPath, '**/*');
+              try {
+                const prompt = `
 							You are a helpful assistant. Try to answer the user query below by using one of your tools. Pay attention to the input required by each tool.
 							## Tools
 							{name: "updateLexicon", input: [old_string,new_string]}
@@ -46,28 +49,32 @@ export function activate(context: vscode.ExtensionContext) {
 							
 							
 							Only respond with a value that matches the Action above
-						`
-						const agentOutput = await promptAgent(prompt)
-						const {name, input} = await parseApiResponse(agentOutput)
-						console.log({name, input})
-						console.log({agentOutput, prompt})
-						if (name === 'updateLexicon') {
-							vscode.window.showInformationMessage('Searching the Lexicon');
-							await updateLexicon(input[0], input[1]);
-						}
-						vscode.window.showInformationMessage('Successfully processed files');
-					} catch (error: any) {
-						vscode.window.showErrorMessage('Error listing files: ' + error.message);
-					}
-				} else {
-					vscode.window.showErrorMessage('No workspace found');
-				}
-			}
-		});
-		
-	});
+						`;
+                const agentOutput = await promptAgent(prompt);
+                const { name, input } = await parseApiResponse(agentOutput);
+                console.log({ name, input });
+                console.log({ agentOutput, prompt });
+                if (name === "updateLexicon") {
+                  vscode.window.showInformationMessage("Searching the Lexicon");
+                  await updateLexicon(input[0], input[1]);
+                }
+                vscode.window.showInformationMessage(
+                  "Successfully processed files",
+                );
+              } catch (error: any) {
+                vscode.window.showErrorMessage(
+                  "Error listing files: " + error.message,
+                );
+              }
+            } else {
+              vscode.window.showErrorMessage("No workspace found");
+            }
+          }
+        });
+    },
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
--- src/utils.ts
+++ src/utils.ts
@@ -1,84 +1,96 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-const tectalicOpenai = require('@tectalic/openai').default;
+const tectalicOpenai = require("@tectalic/openai").default;
 
-
-export function parseApiResponse(response: string): { name: string | null; input: string[] }{
-    try {
-      const match = response.match(/Action:\s*\{\s*"name":\s*"(\w+)",\s*"input":\s*\[(.*?)\]\s*\}/s);
-      if (match) {
-        const name = match[1];
-        const input = match[2].split(',').map(item => item.trim().replace(/^"|"$/g, ''));
-        return { name, input };
-      }
-      throw new Error('Could not parse response:' + response);
-    } catch (error) {
-      console.error('Error parsing response:', error);
-      vscode.window.showErrorMessage('Error parsing response: ' + error);
-      return {name: null, input: []}
+export function parseApiResponse(response: string): {
+  name: string | null;
+  input: string[];
+} {
+  try {
+    const match = response.match(
+        /Action:\s*\{\s*"name":\s*"(\w+)",\s*"input":\s*\[\s*"(.*?)",\s*"(.*?)"\s*\]\s*\}/s,
+    );
+    if (match) {
+      const name = match[1];
+      const input = match[2]
+        .split(",")
+        .map((item) => item.trim().replace(/^"|"$/g, ""));
+      return { name, input };
     }
+    throw new Error("Could not parse response:" + response);
+  } catch (error) {
+    console.error("Error parsing response:", error);
+    vscode.window.showErrorMessage("Error parsing response: " + error);
+    return { name: null, input: [] };
   }
+}
 
 async function createChatCompletionWithOpenAi(promt: string) {
-    try {
-        return await tectalicOpenai(process.env.OPENAI_API_KEY)
-            .chatCompletions.create({
-                model: 'gpt-3.5-turbo',
-                messages: [{ role: 'user', content: promt }]
-            })
-            .then((response: any) => {
-                const responseText = response.data.choices[0].message.content.trim()
-                console.log({response});
-                return responseText;
-            });
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    return await tectalicOpenai(process.env.OPENAI_API_KEY)
+      .chatCompletions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: promt }],
+      })
+      .then((response: any) => {
+        const responseText = response.data.choices[0].message.content.trim();
+        console.log({ response });
+        return responseText;
+      });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const promptAgent = async (prompt: string) => {
-    const response = await createChatCompletionWithOpenAi(prompt);
-    return response;
-}
+  const response = await createChatCompletionWithOpenAi(prompt);
+  return response;
+};
 
-
-export const updateLexicon = async (oldString: string,
-    newString: string) => {
-    if(vscode.workspace.workspaceFolders !== undefined) {
-        const rootPath = vscode.workspace.workspaceFolders[0].uri;
-        try {
-            const lexiconUri = vscode.Uri.joinPath(rootPath, 'project/lexicon.csv');
-            const lexiconContentUint8Array = await vscode.workspace.fs.readFile(lexiconUri);
-            const lexiconContent = new TextDecoder().decode(lexiconContentUint8Array);
-            console.log({lexiconContent});
-            const lines = lexiconContent?.split('\n');
-            // FIXME: this should be finding multiple lines with the old string
-            const indexOfRelevantLines = lines.findIndex((line: string) => {
-                // example value of line: "hello,world" 
-                return line.includes(oldString);
-            });
-            const lineIndex = indexOfRelevantLines; // Replace with the desired line index
-            if (lines.length > lineIndex) {
-                const lineContent = lines[lineIndex];
-                vscode.window.showInformationMessage('Line Content to update: ' + lineContent);
-                const updatedLine = lineContent?.split(',').map((word: string) => {
-                    if (word === oldString) {
-                    return newString;
-                    }
-                    return word;
-                }).join(',');
-
-                lines[lineIndex] = updatedLine;
-                const newContent = Buffer.from(lines.join('\n'));
-                await vscode.workspace.fs.writeFile(lexiconUri, newContent);
-                vscode.window.showInformationMessage('Successfully processed lexicon.csv');
-            } else {
-                vscode.window.showErrorMessage('Line index out of bounds');
+export const updateLexicon = async (oldString: string, newString: string) => {
+  if (vscode.workspace.workspaceFolders !== undefined) {
+    const rootPath = vscode.workspace.workspaceFolders[0].uri;
+    try {
+      const lexiconUri = vscode.Uri.joinPath(rootPath, "project/lexicon.csv");
+      const lexiconContentUint8Array =
+        await vscode.workspace.fs.readFile(lexiconUri);
+      const lexiconContent = new TextDecoder().decode(lexiconContentUint8Array);
+      console.log({ lexiconContent });
+      const lines = lexiconContent?.split("\n");
+      // FIXME: this should be finding multiple lines with the old string
+      const indexOfRelevantLines = lines.findIndex((line: string) => {
+        // example value of line: "hello,world"
+        return line.includes(oldString);
+      });
+      const lineIndex = indexOfRelevantLines; // Replace with the desired line index
+      if (lines.length > lineIndex) {
+        const lineContent = lines[lineIndex];
+        vscode.window.showInformationMessage(
+          "Line Content to update: " + lineContent,
+        );
+        const updatedLine = lineContent
+          ?.split(",")
+          .map((word: string) => {
+            if (word === oldString) {
+              return newString;
             }
-        } catch (error: any) {
-            vscode.window.showErrorMessage('Error processing file: ' + error.message);
-        }
-    } else {
-        vscode.window.showErrorMessage('No workspace found');
+            return word;
+          })
+          .join(",");
+
+        lines[lineIndex] = updatedLine;
+        const newContent = Buffer.from(lines.join("\n"));
+        await vscode.workspace.fs.writeFile(lexiconUri, newContent);
+        vscode.window.showInformationMessage(
+          "Successfully processed lexicon.csv",
+        );
+      } else {
+        vscode.window.showErrorMessage("Line index out of bounds");
+      }
+    } catch (error: any) {
+      vscode.window.showErrorMessage("Error processing file: " + error.message);
     }
-}
+  } else {
+    vscode.window.showErrorMessage("No workspace found");
+  }
+};
