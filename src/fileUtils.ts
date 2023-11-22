85ab9b4bdeb6212ec405f263d6c0cc83ea9decc9
Ben Scholtens <ben.scholtens@me.com>
2023-11-22 10:18:03 -0500

    start refactor for util that creates new files

---
+++ src/fileUtils.ts
@@ -0,0 +1,71 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+import * as vscode from "vscode";
+import { getWorkSpaceFolder } from "./utils";
+const path = require("path");
+
+export const generateFiles = async ({
+  filePath,
+  fileName,
+  fileContent,
+  shouldOverWrite,
+}: {
+  filePath: string;
+  fileName: string;
+  fileContent: string;
+  shouldOverWrite: boolean;
+}) => {
+  const workspaceFolder = getWorkSpaceFolder();
+
+  const newFilePath = vscode.Uri.file(
+    path.join(`${workspaceFolder}${filePath}`, fileName),
+  );
+  const encoder = new TextEncoder();
+
+  let fileSuccessfullyCreated: boolean = false;
+
+  vscode.workspace.fs.stat(newFilePath).then(
+    () => {
+      if (shouldOverWrite) {
+        vscode.workspace.fs
+          .writeFile(newFilePath, encoder.encode(fileContent))
+          .then(
+            () => {
+              fileSuccessfullyCreated = true;
+              vscode.window.showInformationMessage(
+                `${fileName} file overwritten successfully!`,
+              );
+            },
+            (err) => {
+              console.error(`Error: ${err}`);
+              vscode.window.showErrorMessage(
+                `Error overwriting ${fileName} file: ${err.message}`,
+              );
+            },
+          );
+      } else {
+        vscode.window.showInformationMessage(
+          `${fileName} file already exists!`,
+        );
+      }
+    },
+    (err) => {
+      vscode.workspace.fs
+        .writeFile(newFilePath, encoder.encode(fileContent))
+        .then(
+          () => {
+            fileSuccessfullyCreated = true;
+            vscode.window.showInformationMessage(
+              `${fileName} file created successfully!`,
+            );
+          },
+          (err) => {
+            console.error(`Error: ${err}`);
+            vscode.window.showErrorMessage(
+              `Error creating new ${fileName} file: ${err.message}`,
+            );
+          },
+        );
+    },
+  );
+  return fileSuccessfullyCreated;
+};
--- src/initUtils.ts
+++ src/initUtils.ts
@@ -5,41 +5,41 @@ const path = require("path");
 const { exec } = require("child_process");
 
 export const generatePythonEnv = async () => {
-    const pythonInterpreter = "python3";
-    const workspaceFolder = getWorkSpaceFolder();
-    const venvPath = path.join(workspaceFolder, "myenv");
-    const command = `${pythonInterpreter} -m venv ${venvPath}`;
-  
-    const venvPathUri = vscode.Uri.file(venvPath);
-  
-    vscode.workspace.fs.stat(venvPathUri).then(
-      () => {
-        vscode.window.showInformationMessage(
-          "Python virtual env file already exists!",
-        );
-      },
-      (err) => {
-        exec(command, (error: Error, stdout: string, stderr: string) => {
-          if (error) {
-            console.error(`Error: ${error}`);
-            vscode.window.showErrorMessage(
-              `Error creating Python virtual environment: ${error.message}`,
-            );
-          } else {
-            vscode.window.showInformationMessage(
-              "Python virtual environment created successfully!",
-            );
-          }
-        });
-      },
-    );
-  };
-  
-  export const generatePythonFiles = async () => {
-    const workspaceFolder = getWorkSpaceFolder();
-  
-    // After the virtual environment is created, write the embed.py script to a new file
-    const embedScript = `
+  const pythonInterpreter = "python3";
+  const workspaceFolder = getWorkSpaceFolder();
+  const venvPath = path.join(workspaceFolder, "myenv");
+  const command = `${pythonInterpreter} -m venv ${venvPath}`;
+
+  const venvPathUri = vscode.Uri.file(venvPath);
+
+  vscode.workspace.fs.stat(venvPathUri).then(
+    () => {
+      vscode.window.showInformationMessage(
+        "Python virtual env file already exists!",
+      );
+    },
+    (err) => {
+      exec(command, (error: Error, stdout: string, stderr: string) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+          vscode.window.showErrorMessage(
+            `Error creating Python virtual environment: ${error.message}`,
+          );
+        } else {
+          vscode.window.showInformationMessage(
+            "Python virtual environment created successfully!",
+          );
+        }
+      });
+    },
+  );
+};
+
+export const generatePythonFiles = async () => {
+  const workspaceFolder = getWorkSpaceFolder();
+
+  // After the virtual environment is created, write the embed.py script to a new file
+  const embedScript = `
   from sentence_transformers import SentenceTransformer
   import sys
   import json
@@ -54,29 +54,29 @@ export const generatePythonEnv = async () => {
       embedding = generate_embedding(text)
       print(json.dumps(embedding))
   `;
-    const newFilePath = vscode.Uri.file(path.join(workspaceFolder, "embed.py"));
-    const encoder = new TextEncoder();
-  
-    vscode.workspace.fs.stat(newFilePath).then(
-      () => {
-        vscode.window.showInformationMessage("Python file already exists!");
-      },
-      (err) => {
-        vscode.workspace.fs
-          .writeFile(newFilePath, encoder.encode(embedScript))
-          .then(
-            () => {
-              vscode.window.showInformationMessage(
-                "New Python file created successfully!",
-              );
-            },
-            (err) => {
-              console.error(`Error: ${err}`);
-              vscode.window.showErrorMessage(
-                `Error creating new Python file: ${err.message}`,
-              );
-            },
-          );
-      },
-    );
-  };
+  const newFilePath = vscode.Uri.file(path.join(workspaceFolder, "embed.py"));
+  const encoder = new TextEncoder();
+
+  vscode.workspace.fs.stat(newFilePath).then(
+    () => {
+      vscode.window.showInformationMessage("Python file already exists!");
+    },
+    (err) => {
+      vscode.workspace.fs
+        .writeFile(newFilePath, encoder.encode(embedScript))
+        .then(
+          () => {
+            vscode.window.showInformationMessage(
+              "New Python file created successfully!",
+            );
+          },
+          (err) => {
+            console.error(`Error: ${err}`);
+            vscode.window.showErrorMessage(
+              `Error creating new Python file: ${err.message}`,
+            );
+          },
+        );
+    },
+  );
+};
